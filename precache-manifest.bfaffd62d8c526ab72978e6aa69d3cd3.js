9d24898c3c9a6d3110deb638ee8c7ac5be4a0b33
Apurba Barik <72163191+apurbabarik@users.noreply.github.com>
2023-08-04 10:55:17 +0530

    Updates

---
+++ precache-manifest.bfaffd62d8c526ab72978e6aa69d3cd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d622287fca8c87002e91",
+    "url": "/react-app/static/css/main.0ec5ae81.chunk.css"
+  },
+  {
+    "revision": "d622287fca8c87002e91",
+    "url": "/react-app/static/js/main.d622287f.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/react-app/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "3c016175441de394fef9",
+    "url": "/react-app/static/js/2.3c016175.chunk.js"
+  },
+  {
+    "revision": "5012c108abc6db77b728",
+    "url": "/react-app/static/js/runtime~main.5012c108.js"
+  },
+  {
+    "revision": "d8a0f5b17148c11a5e39958d1dba38e4",
+    "url": "/react-app/index.html"
+  }
+];
